ba51e22350c1274696080268389a56cccdf82e01
eifory <bartem007@gmail.com>
2019-12-23 18:34:45 +0300

    some fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import readlineSync from 'readline-sync';
 
-export const welcome = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
-  const userName = getName();
-  console.log('Hello ', userName, '!');
-  return userName;
-}
+const lowNumber = 1;
+const highNumber = 100;
 
 export const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
   return userName;
 }
 
-export const getRandom = (low, high) => {
+export const getRandomNumber = (low, high) => {
   return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
 export const numbersGame = () => {
-  const userName = welcome();
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
+  const userName = getName();
+  console.log('Hello ', userName, '!');
   let counter = 0;
   while (counter !== 3) {
-    let newNumber = getRandom(1, 100);
+    let newNumber = getRandomNumber(lowNumber, highNumber);
     let correctAnswer = (newNumber % 2 === 0) ? 'yes' : 'no';
-    console.log('Question: ', newNumber);
+    console.log(`Question: ${newNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
